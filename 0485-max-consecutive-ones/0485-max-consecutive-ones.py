c1bac90451edb65d1b8e4f55f1a3f5c64535a25e
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-01-20 16:50:40 -0500

    Time: 39 ms (16.92%), Space: 20.2 MB (13.6%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -4,12 +4,21 @@ class Solution:
         length = 0
         max_length = 0
 
-        for i in range(n):
-            if nums[i] == 1:
-                length += 1
-                max_length = max(max_length, length)
-            else: 
-                length = 0
+        # for i in range(n):
+        #     if nums[i] == 1:
+        #         length += 1
+        #         max_length = max(max_length, length)
+        #     else: 
+        #         length = 0
+
+        l = 0
+        
+        for r in range(n):
+            if nums[r]==0:
+                l = r + 1
+            w = r - l + 1
+            max_length = max(max_length, w)
+                
             
         return max_length
         
