2817ba6937fd919844a898acf9d05cebdfab5b9d
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-01-25 17:59:25 -0500

    Time: 0 ms (100%), Space: 18.2 MB (18.74%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,41 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        sol = []
-        hmap = {'(':')'}
-        stk = []
+        res, sol = [], []
 
-        # def validParenthesis():
-        #     for i in range(n):
-        #         if i in hmap:
-        #             stk.append(i)
-        #         elif stk and i == hmap[stk[-1]]:
-        #             stk.pop()
-        #         else:
-        #             return False
+        def backtrack(open, close):
+            if len(sol) == 2*n:
+                res.append(''.join(sol))
+                return 
             
-        #     return not stk
-
-        def backtrack(left, right, i):
-            if len(i) == 2*n:
-                res.append(i)
-                return  
+            if open < n:
+                sol.append('(')
+                backtrack(open + 1, close)
+                sol.pop()
             
-            if left < n: 
-                #sol.append()
-                backtrack(left + 1, right, i + '(')
-            if right < left:
-                #sol.append()
-                backtrack(left, right + 1, i + ')')
-
-        backtrack(0, 0, '')
+            if open > close:
+                sol.append(')')
+                backtrack(open, close + 1)
+                sol.pop()
+        
+        backtrack(0, 0)
         return res
-
-
-
-
-            
-                
-
-            
         
