84974116374e5b94d3830abc2ff799a9edde3600
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-01-26 19:18:39 -0500

    Time: 233 ms (86.74%), Space: 20.3 MB (52.69%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        m, n = len(grid), len(grid[0])
+
+        graph = defaultdict(list)
+
+        def dfs(i, j):
+            if i >= m or j >= n or i < 0 or j < 0 or grid[i][j] != '1':
+                return 
+            
+            char = grid[i][j]
+            grid[i][j] = '0'
+
+            if dfs(i+1, j) or dfs(i, j+1) or dfs(i-1, j) or dfs(i, j-1):
+                return True
+
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    islands += 1
+                    dfs(i, j)
+        
+        return islands
+                    
+        
+
+
+
+
+        
