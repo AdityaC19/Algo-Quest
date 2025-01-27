d70508f51a07911d90d20147d34e4d30a86b742f
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-01-27 00:12:15 -0500

    Time: 3 ms (78.57%), Space: 19.4 MB (28.64%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+
+        for u, v in prerequisites:
+            graph[u].append(v)
+        
+        print(graph)
+
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+        states = [UNVISITED] * numCourses
+        ans = []
+
+        def dfs(i):
+            state = states[i]
+            if state == VISITED: return True # verified
+            elif state == VISITING: return False # in loop
+
+            states[i] = VISITING
+
+            for nei_node in graph[i]:    
+                if not dfs(nei_node):   # in loop
+                    return False
+
+            states[i] = VISITED
+            ans.append(i)  
+            return True  
+        
+            
+        # for i in range(numCourses):
+        #     if dfs(i):
+        #         pass
+        
+        return ans if all(dfs(i) for i in range(numCourses)) else []
+
+        
+        
+        
