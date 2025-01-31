a8a574bdf34b3b688b1d4665b75fd1b81dfc6aa3
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-01-30 20:25:18 -0500

    Time: 395 ms (48.95%), Space: 19.6 MB (49.61%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
@@ -9,16 +9,6 @@ class Solution:
         diff = 0
         max_diff = 0
 
-        # graph = defaultdict(list)
-
-        # for i in range(m):
-        #     for j in range(n):
-        #         for r,c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-        #             if 0 <= r < m and 0 <= c < n:
-        #                 graph[(i,j)].append((r, c, abs(heights[i][j] - heights[r][c])))
-        
-        
-
 
         min_heap = [(0,0,0)] # (effort, i, j)
 
@@ -36,28 +26,7 @@ class Solution:
                         heapq.heappush(min_heap, (new_diff, r,c))
                         seen[(r, c)] = new_diff
         
-        for key, value in seen.items():
-            print(key, value) 
-        
         return max_diff
 
-
-        # while q:
-        #     for _ in range(len(q)):
-        #         i, j = q.popleft()
-        #         max_diff = max(max_diff, diff)
-
-        #         if (i, j) == (n - 1, n - 1):
-        #             return max_diff
-
-        #         for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-        #             if 0 <= r < m and 0 <= c < n:
-        #                 diff = abs(heights[r][c] - heights[i][j])
-        #                 if diff < seen[(r, c)]:  
-        #                     q.append((r, c))
-        #                     seen[(r, c)] = diff
-                        
-        
-        return max_diff
 
                 
