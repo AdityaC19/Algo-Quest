e216da4f15d2932eb925209fd7c0473c6de6ac67
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-02-01 21:29:35 -0500

    Time: 0 ms (100%), Space: 17.8 MB (51.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,13 +5,13 @@ class Solution:
         
         dp = [[1] * n for _ in range(m)]
 
-        dp[0][1] = 1
-        dp[1][0] = 1
+        # dp[0][1] = 1
+        # dp[1][0] = 1
 
         for i in range(1,m):
             for j in range(1,n):
                 dp[i][j] =  dp[i][j-1] + dp[i-1][j]
-        print(dp)
+        #print(dp)
         
         return dp[m-1][n-1]
             
