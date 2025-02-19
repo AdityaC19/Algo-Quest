837a77bbf7eada23959968543a00787de1b2d264
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-02-19 18:16:46 -0500

    Time: 3037 ms (89.9%), Space: 17.7 MB (87.12%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        res = []
-        index_complete = []
         m, n = len(board), len(board[0])
 
-        def backtrack(i,j, k):
+        def dfs(i, j, k):
             if k == len(word):
                 return True
+
+            # out of bounds conditions or word not found
             if i >= m or j >= n or i < 0 or j < 0 or board[i][j] != word[k]:
                 return False
             
-            char = board[i][j]
+            # mark visited cell
             board[i][j] = '#'
 
-            if backtrack(i, j+1, k+1) or backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j -1, k+1):
-                return True  
+            # run dfs in all possible directions
+            if dfs(i+1, j, k+1) or dfs(i, j+1, k+1) or dfs(i-1, j, k+1) or dfs(i, j-1, k+1):
+                return True
+            
+            # unmark the cell for backtracking
+            board[i][j] = word[k]
 
-            board[i][j] = char
-            return False      
+            return False
 
         for a in range(m):
             for b in range(n):
-                if backtrack(a,b,0):
+                if dfs(a,b,0):
                     return True
 
         return False
         
-     
-
-            
-                        
-
-
         
