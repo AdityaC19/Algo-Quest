faa12692c102920984d5b308504c8f1d0c90b74e
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-02-20 17:30:10 -0500

    Time: 3 ms (82.88%), Space: 18 MB (26.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,20 +1,21 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
         m, n = len(grid), len(grid[0])
+        fresh_oranges = 0
         time = 0
-        fresh = 0
 
-        q = deque() 
+        seen = set()
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    q.append((i,j))
+                    q.append((i, j))
                 if grid[i][j] == 1:
-                    fresh += 1
-        
-        if fresh == 0:
+                    fresh_oranges += 1
+
+        if fresh_oranges == 0:
             return 0
         
         while q:
@@ -24,11 +25,20 @@ class Solution:
                 for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
                     if 0 <= r < m and 0 <= c < n and grid[r][c] == 1:
                         grid[r][c] = 2
-                        fresh -= 1
-                        if fresh == 0:
+                        #print(grid[r][c])
+                        print(fresh_oranges)
+                        fresh_oranges -= 1
+
+                        if fresh_oranges == 0:
                             return time
-
+                        
+                        seen.add((r,c))
                         q.append((r,c))
-    
+                    
         return -1
+                
+                
 
+
+                
+        
