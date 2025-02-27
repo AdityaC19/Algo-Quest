6bc38a27401768b28482a662ecdaf4d5923a1fcb
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-02-27 17:21:03 -0500

    Time: 3 ms (91.33%), Space: 19.1 MB (70.37%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -5,87 +5,27 @@ class Solution:
 
         for u, v, price in flights:
             graph[u].append((v, price))
-            #graph[v].append((u, price))
-        
-        min_heap = [(0, src, 0)]
-        #q.append((0, src, 0))
-        seen = defaultdict(int)
+                
+        q = deque([(0, src, 0)])    # (price, node, stops)
+        cost = [float('inf')] * n
+        cost[src] = 0
 
-        while min_heap:
-            price, i, stops = heapq.heappop(min_heap)
-            if i == dst: 
-                return price
-            
-            if stops > k:
+        while q:
+            price, node, stops = q.popleft()
+            # if node == dst:
+            #     return price
+
+            if stops> k:
                 continue
-
-            for nei, nei_price in graph[i]:
-                new_price = price + nei_price
-                if (nei, stops) not in seen or new_price < seen[(nei, stops)]:
-                    seen[(nei, stops)] = new_price
-                    heapq.heappush(min_heap, (new_price, nei, stops+1))
-                    #print(min_heap)
-        
-        return -1
-
-
-
-        
-
-        # while min_heap:
-        #     price, i = heapq.heappop(min_heap)
-        #     if i in seen:
-        #         continue
-        #     seen[i] = price
-
-        #     # for j in range(k):
-        #     #     if j not in seen:
-        #     #         heapq.heappush(min_heap, ())
-
-        #     for nei, nei_price in graph[i]:
-        #         if nei not in seen:
-        #             heapq.heappush(min_heap, (nei_price+price, nei))
-        
-        # print(seen)
-
-
-
-# from collections import defaultdict, deque
-# from typing import List
-
-# class Solution:
-#     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         graph = defaultdict(list)
-
-#         for u, v, price in flights:
-#             graph[u].append((v, price))
-        
-#         stk = []
-#         def topoSort(node):
-#             visited[node] = 1
             
-#             for nei in graph[node]:
-#                 v = nei[0]
-#                 if not visited[v]:
-#                     topoSort(v)
-
-#             stk.append(node)
-
-#         visited = [0] * n
-#         for i in range(n):
-#             if not visited[i]:
-#                 topoSort(i)
+            for nei, wt in graph[node]:
+                new_price = price + wt
+                if new_price < cost[nei]:
+                    cost[nei] = new_price
+                    q.append((new_price, nei, stops+1))
         
-#         dist = [float('inf')] * n
-#         dist[src] = 0  # Source node distance is 0
+        return cost[dst] if cost[dst] != float('inf') else -1
 
-#         while stk:
-#             node = stk.pop()
 
-#             if dist[node] != float('inf'):  
-#                 for nei in graph[node]:
-#                     v, price = nei
-#                     if dist[node] + price < dist[v]:
-#                         dist[v] = dist[node] + price
-
-#         return dist[dst] if dist[dst] != float('inf') else -1
+        
+        
