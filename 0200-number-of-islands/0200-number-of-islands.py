62faa2cebdcaeded39b668ded275f5680e9985f2
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-03-01 17:38:45 -0500

    Time: 236 ms (83.22%), Space: 20 MB (82.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,12 +5,14 @@ class Solution:
 
         def dfs(i, j):
             if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != '1':
-                return False
+                return 
 
-            grid[i][j] = '#'
+            grid[i][j] = '0'
             
-            if dfs(i+1, j) or dfs(i, j+1) or dfs(i-1,j) or dfs(i, j-1):
-                return True
+            dfs(i+1, j)
+            dfs(i, j+1)
+            dfs(i-1,j)
+            dfs(i, j-1)
         
         for i in range(m):
             for j in range(n):
