855492f691daf36e2c4233f1b5868114f2b6a263
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-03-01 18:31:16 -0500

    Time: 7 ms (27.79%), Space: 17.7 MB (93.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,44 +1,40 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
         m, n = len(grid), len(grid[0])
-        fresh_oranges = 0
-        time = 0
-
+        
+        q = deque()
         seen = set()
+        fresh = 0
+        time = 0
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    q.append((i, j))
+                    q.append((i,j))
                 if grid[i][j] == 1:
-                    fresh_oranges += 1
-
-        if fresh_oranges == 0:
-            return 0
+                    fresh += 1
         
+        if fresh == 0:
+            return 0
+
         while q:
             time += 1
             for _ in range(len(q)):
                 i, j = q.popleft()
-                for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                for r,c in [(i+1,j), (i, j+1), (i-1,j), (i,j-1)]:
                     if 0 <= r < m and 0 <= c < n and grid[r][c] == 1 and (r,c) not in seen:
                         grid[r][c] = 2
-                        #print(grid[r][c])
-                        #print(fresh_oranges)
-                        fresh_oranges -= 1
+                        fresh -= 1
 
-                        if fresh_oranges == 0:
+                        if fresh == 0:
                             return time
-                        
-                        seen.add((r,c))
+
                         q.append((r,c))
-                    
+                        seen.add((r,c))
+        
         return -1
-                
-                
 
 
-                
+
         
