1e27a9160f1995748305c2d27a34ce6a56a257bf
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-03-01 21:29:38 -0500

    Time: 23 ms (94.78%), Space: 19.1 MB (91.75%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,45 +1,42 @@
-from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
+        pac_q = deque()
+        pac_seen = set()
 
-        p_que = deque()
-        p_seen = set()
+        atl_q = deque()
+        atl_seen = set()
 
-        a_que = deque()
-        a_seen = set()
-
-        # TOP WALL
-        for j in range(n):
-            p_que.append((0, j))
-            p_seen.add((0, j))
+        for i in range(n):
+            pac_q.append((0,i))
+            pac_seen.add((0,i))
         
-        # LEFT WALL
-        for i in range(1, m):
-            p_que.append((i, 0))
-            p_seen.add((i, 0))
-        
-        # RIGHT WALL
         for i in range(m):
-            a_que.append((i, n-1))
-            a_seen.add((i, n-1))
+            pac_q.append((i,0))
+            pac_seen.add((i,0))
         
-        # BOTTOM WALL
-        for j in range(n-1):
-            a_que.append((m-1, j))
-            a_seen.add((m-1, j))
-
-        def get_coords(que, seen):
-            while que:
-                i, j = que.popleft()
-                for r,c in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r, c) not in seen:
-                        seen.add((r,c))
-                        que.append((r,c))
-                
-        get_coords(p_que, p_seen)
-        get_coords(a_que, a_seen)
-
-        return list(p_seen.intersection(a_seen))
+        for i in range(n):
+            atl_q.append((m-1,i))
+            atl_seen.add((m-1,i))
         
+        for i in range(m):
+            atl_q.append((i,n-1))
+            atl_seen.add((i,n-1))
+        
+        def bfs(q, seen):
+            while q:
+                for _ in range(len(q)):
+                    (i, j) = q.popleft()
+                    for r,c in [(i, j+1), (i+1, j), (i-1, j), (i, j-1)]:
+                        if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r,c) not in seen:
+                            q.append((r,c))
+                            seen.add((r,c))
+        
+        bfs(pac_q, pac_seen)
+        bfs(atl_q, atl_seen)
+        
+        return list(pac_seen & atl_seen)
+
+                     
+
         
