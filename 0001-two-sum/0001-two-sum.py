5c21edce604984ed104c601071437f5d0a3621ed
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-04-24 17:55:15 -0400

    Time: 4 ms (41.09%), Space: 19 MB (34.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,38 +7,8 @@ class Solution:
             if x in hmap:
                 return [i, hmap[x]]
             hmap[nums[i]] = i
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        '''
-        map = {}
-        n = len(nums)
-
-        for i in range(n):
-            x = target - nums[i]
-            if x in map:
-                return [map[x], i]
-            map[nums[i]] = i
-        
-        return []
-        '''
-                
             
+
+
+
         
