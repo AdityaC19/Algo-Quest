b00420a3adf57a6005e28a13bd19e46b85267bd9
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-04-28 20:05:08 -0400

    Time: 307 ms (88.2%), Space: 96.7 MB (61.09%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,16 +1,8 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # 1. DFS with recursion
-        # 2. DFS with Stack (iteratively)
-        # 3. BFS with deque
-
         graph = defaultdict(list)
 
-        if source == destination:
-            return True
-
-        # Adjacency matrix with dict
-        for u, v in edges:
+        for u,v in edges:
             graph[u].append(v)
             graph[v].append(u)
         
@@ -27,22 +19,5 @@ class Solution:
                 if nei_node not in seen:
                     seen.add(nei_node)
                     q.append(nei_node)
+        
         return False
-
-        # def dfs(i):
-        #     if i == destination:
-        #         return True
-
-        #     for nei_node in graph[i]:
-        #         if nei_node not in seen:
-        #             seen.add(nei_node)
-        #             if dfs(nei_node):
-        #                 return True
-        
-        #     return False
-        
-        #return dfs(source)
-
-
-
-
