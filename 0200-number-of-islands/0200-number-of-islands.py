440bb704a1ef37830557318028b3c447bd7de869
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-24 14:51:27 -0400

    Time: 283 ms (21.45%), Space: 25 MB (33.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,18 +3,17 @@ class Solution:
         m, n = len(grid), len(grid[0])
         islands = 0
 
-        
         seen = set()
 
         def bfs(i, j):
             q = deque([(i,j)])
-            while q:
-                i, j = q.popleft()
-                for r,c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and grid[r][c] == '1' and (r,c) not in seen:
-                        seen.add((r,c))
-                        q.append((r,c))
 
+            while q:
+                i,j = q.popleft()
+                for r, c in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == '1':
+                        q.append((r,c))
+                        seen.add((r,c))
         
         for i in range(m):
             for j in range(n):
@@ -22,7 +21,9 @@ class Solution:
                     seen.add((i,j))
                     islands += 1
                     bfs(i,j)
-        
+
         return islands
+    
+
 
         
