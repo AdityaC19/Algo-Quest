e141caf9be65e09e4b9b00aea91e80c535fa90d2
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-24 18:09:21 -0400

    Time: 1 ms (89.98%), Space: 18 MB (26.96%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,10 +1,10 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        time = 0
-        fresh = 0
-
         q = deque()
+
+        graph = defaultdict(list)
+        fresh_oranges = 0
         seen = set()
 
         for i in range(m):
@@ -12,26 +12,25 @@ class Solution:
                 if grid[i][j] == 2:
                     q.append((i,j))
                 if grid[i][j] == 1:
-                    fresh += 1
+                    fresh_oranges += 1
         
-        if fresh == 0:
+        if fresh_oranges == 0:
             return 0
         
+        time = 0
         while q:
             time += 1
             for _ in range(len(q)):
                 i, j = q.popleft()
-                for r,c in [(i,j+1), (i+1,j), (i-1,j), (i,j-1)]:
-                    if 0 <= r < m and 0 <= c < n and grid[r][c] == 1 and (r,c) not in seen:
-                        grid[r][c] = 2
-                        fresh -= 1
-                        if fresh == 0:
+                for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == 1:
+                        fresh_oranges -= 1
+                        if fresh_oranges == 0:
                             return time
                         
-                        q.append((r,c))
                         seen.add((r,c))
-            
+                        q.append((r,c))
+        
         return -1
-
 
         
