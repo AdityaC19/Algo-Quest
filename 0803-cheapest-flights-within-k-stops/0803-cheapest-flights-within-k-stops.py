528546ac47fd945d4c46579198cb685750894f30
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-25 17:17:19 -0400

    Time: 23 ms (50.14%), Space: 19.4 MB (27.72%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -1,14 +1,25 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dist = [float('inf')] * n
-        dist[src] = 0
+        graph = defaultdict(list)
 
-        for _ in range(k+1):
-            temp = dist[:]
+        for u, v, price in flights:
+            graph[u].append((v, price))
+        
+        cost = [float('inf')] * (n+1)
+        cost[src] = 0
+        q = deque([(0, src, 0)])    # (price, node, stops)
 
-            for u, v, price in flights:
-                if dist[u] != float('inf') and dist[u] + price < temp[v]:
-                    temp[v] = dist[u] + price
-            dist = temp
+        while q:
+            price, node, stops = q.popleft()
 
-        return dist[dst] if dist[dst] != float('inf') else -1
+            if stops > k:
+                continue
+
+            for nei_node, wt in graph[node]:
+                new_price = price + wt
+                if new_price < cost[nei_node]:
+                    cost[nei_node] = new_price
+                    q.append((new_price, nei_node, stops+1))
+                    print(q)
+        
+        return cost[dst] if cost[dst] != float('inf') else -1
