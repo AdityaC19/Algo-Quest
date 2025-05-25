ed78910a0ba4360fd79985260dbdec9c2e7a6635
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-24 23:58:08 -0400

    Time: 23 ms (94.55%), Space: 19.2 MB (91.56%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,42 +1,48 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
-        pac_q = deque()
         pac_seen = set()
-
-        atl_q = deque()
         atl_seen = set()
 
-        for i in range(n):
-            pac_q.append((0,i))
-            pac_seen.add((0,i))
-        
+        pac_q = deque()
+        atl_q = deque()
+
         for i in range(m):
             pac_q.append((i,0))
             pac_seen.add((i,0))
         
         for i in range(n):
-            atl_q.append((m-1,i))
-            atl_seen.add((m-1,i))
+            pac_q.append((0,i))
+            pac_seen.add((0,i))
         
         for i in range(m):
-            atl_q.append((i,n-1))
-            atl_seen.add((i,n-1))
+            atl_q.append((i, n-1))
+            atl_seen.add((i, n-1))
+        
+        for i in range(n):
+            atl_q.append((m-1, i))
+            atl_seen.add((m-1, i))
         
         def bfs(q, seen):
             while q:
                 for _ in range(len(q)):
                     (i, j) = q.popleft()
-                    for r,c in [(i, j+1), (i+1, j), (i-1, j), (i, j-1)]:
-                        if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r,c) not in seen:
+                    for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                        if 0 <= r < m and 0 <= c < n and (r,c) not in seen and heights[r][c] >= heights[i][j]:
                             q.append((r,c))
                             seen.add((r,c))
         
         bfs(pac_q, pac_seen)
         bfs(atl_q, atl_seen)
-        
-        return list(pac_seen & atl_seen)
 
-                     
+        return list(pac_seen & atl_seen)
+        #return list(result)
+        
+
+
+
+
+
+
 
         
