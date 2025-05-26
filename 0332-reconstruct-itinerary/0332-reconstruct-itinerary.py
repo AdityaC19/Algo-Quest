76c8a23efb6326bcbb80c758c28f4120eeca3bec
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-26 01:41:59 -0400

    Time: 7 ms (18.33%), Space: 18.1 MB (49.49%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+        
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+        
+        print(graph)
+            
+        itinerary = []
+        def dfs(node):
+            while graph[node]:
+                dfs(graph[node].pop())
+            itinerary.append(node)
+        
+        dfs("JFK")
+        
+        return itinerary[::-1]
