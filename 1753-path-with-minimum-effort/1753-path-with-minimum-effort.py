53b7c04d7d61acbd8615db06aeccd79bf8da1889
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-26 01:40:45 -0400

    Time: 387 ms (44.96%), Space: 19.7 MB (49.75%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.py
@@ -1,32 +1,22 @@
-from collections import deque
-import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        m,n = len(heights), len(heights[0])
-        q = deque()
-        q.append((0,0))
-        seen = defaultdict(lambda: float('inf'))
-        diff = 0
-        max_diff = 0
-
-
-        min_heap = [(0,0,0)] # (effort, i, j)
+        m, n = len(heights), len(heights[0])
+        min_heap = [(0, 0, 0)]  # (effort, i, j)
+        dist = defaultdict(lambda: float('inf'))
 
         while min_heap:
-            diff, i, j = heapq.heappop(min_heap)
-            max_diff = max(max_diff, diff)
+            effort, i, j = heapq.heappop(min_heap)
 
-            if (i,j) == (m-1, n-1):
-                break
+            if (i, j) == (m-1, n-1):
+                return effort
             
-            for r,c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+            for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
                 if 0 <= r < m and 0 <= c < n:
-                    new_diff = max(diff, abs(heights[r][c] - heights[i][j]))
-                    if new_diff < seen[(r, c)]:  
-                        heapq.heappush(min_heap, (new_diff, r,c))
-                        seen[(r, c)] = new_diff
+                    new_dist = max(effort, abs(heights[i][j] - heights[r][c]))
+                    if new_dist < dist[(r,c)]:
+                        dist[(r,c)] = new_dist
+                        heapq.heappush(min_heap, (new_dist, r, c)) 
+
+
         
-        return max_diff
-
-
-                
+        
