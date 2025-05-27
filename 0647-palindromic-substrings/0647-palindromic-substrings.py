77830c3845441d7a50e87fbe48bd02d85e36b97a
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-26 22:18:03 -0400

    Time: 105 ms (91.79%), Space: 17.7 MB (87.98%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,18 +3,19 @@ class Solution:
         n = len(s)
         res = 0
 
-        def countPalindrome(s, l, r):
-            res = 0
+        for i in range(n):
+            l, r = i, i
             while l >= 0 and r < n and s[l] == s[r]:
                 res += 1
                 l -= 1
                 r += 1
-            return res
-
-        for i in range(n):
-            res += countPalindrome(s, i, i)
-            res += countPalindrome(s, i, i+1)
-
+            
+            l, r = i, i+1
+            while l >= 0 and r < n and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        
         return res
 
 
