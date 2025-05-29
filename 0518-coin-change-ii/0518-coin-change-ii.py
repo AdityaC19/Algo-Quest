ea1ccf92be93ce35085662522fe5ebe6d7b8516b
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-28 22:00:19 -0400

    Time: 127 ms (74.5%), Space: 17.9 MB (92.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,16 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        coins.sort()
-        dp = [[0] * (amount + 1) for _ in range(n + 1)]
-        
-        for i in range(n + 1):
-            dp[i][0] = 1
-        
-        for i in range(n - 1, -1, -1):
-            for a in range(amount + 1):
-                if a >= coins[i]:
-                    dp[i][a] = dp[i + 1][a]  
-                    dp[i][a] += dp[i][a - coins[i]]  
+        dp = [0] * (amount + 1)
+        dp[0] = 1
 
-        return dp[0][amount]
+        for i in range(n):
+            for a in range(coins[i] , amount +1):
+                dp[a] += dp[a - coins[i]]
+        return dp[amount]
