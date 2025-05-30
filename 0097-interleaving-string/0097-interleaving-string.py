c4a5cc983ce3195fd103e9430dd22ea579b835da
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-30 17:16:09 -0400

    Time: 49 ms (50.88%), Space: 17.8 MB (73.96%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,29 +1,19 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        memo = {}
-
-        def dfs(i, j):
-            if i == len(s1) and j == len(s2):
-                return True
-            
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            if i < len(s1) and s1[i] == s3[i+j]:
-                if dfs(i+1, j):
-                    return True
-            
-            if j < len(s2) and s2[j] == s3[i+j]:
-                if dfs(i, j+1):
-                    return True
-            
-            memo[(i, j)] = False
-            
-            return memo[(i, j)]
+        m, n = len(s1), len(s2)
         
-        if len(s1) + len(s2) != len(s3):
+        if m + n != len(s3):
             return False
 
-        return dfs(0,0)
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp[0][0] = True
 
+        for i in range(m+1):
+            for j in range(n+1):
+                if i > 0 and s1[i-1] == s3[i+j-1] and dp[i-1][j] == True:
+                    dp[i][j] = True
+                if j > 0 and s2[j-1] == s3[i+j-1] and dp[i][j-1] == True:
+                    dp[i][j] = True
+        
+        return dp[m][n]
         
