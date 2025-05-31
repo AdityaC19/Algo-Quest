c12ce5d644ab536b71376d7b7a463dd2c3914ea4
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-31 17:22:31 -0400

    Time: 195 ms (34.63%), Space: 38.4 MB (36.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,19 +2,20 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         memo = {}
-
+         
         def dfs(i, curSum):
-            if i == n:
+            if i >= n :
                 return 1 if curSum == target else 0
             
-            if (i, curSum) in memo:
-                return memo[(i, curSum)]
-
-            add = dfs(i + 1, curSum + nums[i])
-            sub = dfs(i + 1, curSum - nums[i])
-
-            result = add + sub
-            memo[(i, curSum)] = result
-            return memo[(i, curSum)]
+            if (i,curSum) in memo:
+                return memo[(i,curSum)]
             
+            add = dfs(i+1, curSum + nums[i])
+            sub = dfs(i+1, curSum - nums[i])
+
+            memo[(i,curSum)] = add + sub
+            return memo[(i,curSum)]
+        
         return dfs(0,0)
+
+        
