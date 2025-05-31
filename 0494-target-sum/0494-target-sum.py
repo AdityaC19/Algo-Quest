444c112e3c4dda2952314315e1bc988d5dbc3259
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-31 17:28:59 -0400

    Time: 123 ms (64.22%), Space: 18.4 MB (54.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,21 +1,31 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        memo = {}
-         
-        def dfs(i, curSum):
-            if i >= n :
-                return 1 if curSum == target else 0
-            
-            if (i,curSum) in memo:
-                return memo[(i,curSum)]
-            
-            add = dfs(i+1, curSum + nums[i])
-            sub = dfs(i+1, curSum - nums[i])
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        dp[0][0] = 1 # When nums is empty and target is 0, there is only one possible way
 
-            memo[(i,curSum)] = add + sub
-            return memo[(i,curSum)]
+        for i in range(len(nums)):
+            for curr_sum, count in dp[i].items():
+                dp[i+1][curr_sum - nums[i]] += count
+                dp[i+1][curr_sum + nums[i]] += count
         
-        return dfs(0,0)
+        return dp[len(nums)][target]
+        
+        # n = len(nums)
+        # memo = {}
+         
+        # def dfs(i, curSum):
+        #     if i >= n :
+        #         return 1 if curSum == target else 0
+            
+        #     if (i,curSum) in memo:
+        #         return memo[(i,curSum)]
+            
+        #     add = dfs(i+1, curSum + nums[i])
+        #     sub = dfs(i+1, curSum - nums[i])
+
+        #     memo[(i,curSum)] = add + sub
+        #     return memo[(i,curSum)]
+        
+        # return dfs(0,0)
 
         
