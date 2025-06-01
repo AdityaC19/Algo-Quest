0d5e228f9ebfde10170e98215c5410defb8d940d
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-05-31 20:39:02 -0400

    Time: 63 ms (91.02%), Space: 51.4 MB (21.98%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -7,8 +7,11 @@ class Solution:
         prev = 0
 
         for i in range(1,n):
+            # if start time of curr meeting > end time of prev meeting
             if intervals[i][0] >= intervals[prev][1]:
+                # set prev to curr idx i
                 prev = i
+                # increment safe meetings counter by 1
                 count += 1
                 
         return n - count 
