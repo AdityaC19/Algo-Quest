d6afa89148d416b29a5b0a2cdf5ea004eb40cab0
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 17:13:26 -0400

    Time: 5 ms (20.9%), Space: 18.4 MB (23.82%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,36 +1,36 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # Make parent and rank lists
         n = len(edges)
         parent = [i for i in range(n+1)]
         rank = [0] * (n+1)
 
         def find(node):
-            if node == parent[node]:    # this is the root parent
-                return node
-            parent[node] = find(parent[node])   # Path compression
+            if node == parent[node]:
+                return parent[node]
+            parent[node] = find(parent[node])   # path compression
             return parent[node]
-
         
         def union(u, v):
-            root_u = find(u)
-            root_v = find(v)
-
-            if root_u == root_v:
-                return False        # Cycle detected (redundant edge)
+            rootU, rootV = find(u), find(v)
+            if rootU == rootV:
+                return False
             
-            if rank[root_u] > rank[root_v]:
-                parent[root_v] = root_u
-            elif rank[root_u] < rank[root_v]:
-                parent[root_u] = root_v
+            # if rank[rootU] > rank[rootV]: rootU will become parent of rootV
+            if rank[rootU] > rank[rootV]:   
+                parent[rootV] = rootU
+                rank[rootU] += rank[rootV]
             else:
-                parent[root_v] = root_u
-                rank[root_u] += 1
+                parent[rootU] = rootV
+                rank[rootV] += rank[rootU]
             
-            return True             # Union successful
+            return True
         
         for u, v in edges:
             if not union(u,v):
-                return [u,v]        # First edge that forms a cycle
-        
-        return []          # No redundant edge found
+                return [u,v]
+        return []
+
+
+            
         
