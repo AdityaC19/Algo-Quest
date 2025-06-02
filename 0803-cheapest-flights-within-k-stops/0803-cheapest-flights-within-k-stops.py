efa5c15bbd699308d776f794ebf301ff3012c233
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 18:51:18 -0400

    Time: 23 ms (49.72%), Space: 19.5 MB (20.03%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.py
@@ -3,23 +3,24 @@ class Solution:
         graph = defaultdict(list)
 
         for u, v, price in flights:
-            graph[u].append((v, price))
+            graph[u].append((v,price))
         
-        cost = [float('inf')] * (n+1)
-        cost[src] = 0
-        q = deque([(0, src, 0)])    # (price, node, stops)
+        min_heap = [(0, src, 0)]  # (price, node, stops)
+        cost = defaultdict(int) # key=node, stops    val=cost
 
-        while q:
-            price, node, stops = q.popleft()
-
+        while min_heap:
+            price, node, stops = heapq.heappop(min_heap)
+            if node == dst:
+                return price
             if stops > k:
                 continue
-
             for nei_node, wt in graph[node]:
-                new_price = price + wt
-                if new_price < cost[nei_node]:
-                    cost[nei_node] = new_price
-                    q.append((new_price, nei_node, stops+1))
-                    print(q)
+                new_cost = price + wt
+                if (nei_node, stops) not in cost or new_cost < cost[(nei_node, stops)] :
+                    cost[(nei_node, stops)] = new_cost
+                    heapq.heappush(min_heap, (new_cost, nei_node, stops+1))
         
-        return cost[dst] if cost[dst] != float('inf') else -1
+        return -1
+
+            
+        
