4865b9ee0cb6384655736d8726e399893a3e2e08
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 15:29:19 -0400

    Time: 4 ms (48.51%), Space: 17.8 MB (44.9%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,21 +1,19 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        q = deque()
 
-        graph = defaultdict(list)
-        fresh_oranges = 0
+        q = deque()
         seen = set()
 
+        fresh_oranges = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i,j))
                 if grid[i][j] == 1:
                     fresh_oranges += 1
-        
-        if fresh_oranges == 0:
-            return 0
+
+        if fresh_oranges == 0: return 0
         
         time = 0
         while q:
@@ -23,14 +21,19 @@ class Solution:
             for _ in range(len(q)):
                 i, j = q.popleft()
                 for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == 1:
+                    if 0 <= r < m and 0 <= c < n and grid[r][c] == 1 and (r,c) not in seen:
+                        grid[r][c] = 2
                         fresh_oranges -= 1
                         if fresh_oranges == 0:
-                            return time
-                        
-                        seen.add((r,c))
+                            return time 
                         q.append((r,c))
+                        seen.add((r,c))
+            
         
         return -1
+
+
+
+
 
         
