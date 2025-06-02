d46d2c1c56c66d0fc3edf227d230c2e914ffda70
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 16:01:26 -0400

    Time: 3 ms (90.81%), Space: 21.6 MB (91.06%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,44 +5,35 @@ class Solution:
         """
         m, n = len(board), len(board[0])
         q = deque()
-        #safe = set()
+        safe = set()
 
-        # Mark Os at the boudary (marking as #)
+        # Mark all boundary O as safe:
         for i in range(m):
-            for j in [0, n-1]:  # First and last columns
+            for j in [0, n-1]:  # first and last col
                 if board[i][j] == 'O':
-                    board[i][j] = "#"
-                    q.append((i, j))
-                    #safe.add((i, j))
-
-        for j in range(n):
-            for i in [0, m-1]:  # First and last rows
-                if board[i][j] == 'O':
-                    board[i][j] = "#"
-                    q.append((i, j))
-                    #safe.add((i, j))
+                    q.append((i,j))
+                    safe.add((i,j))
         
-        # Perform BFS to mark boundary reachable Os as safe (marking as #)
+        for i in [0, m-1]:  # First and last row
+            for j in range(n):
+                if board[i][j] == 'O':
+                    q.append((i,j))
+                    safe.add((i,j))
+        
+        # Use BFS to mark safe Os
         while q:
-            # for _ in range(len(q)):   # level wise traveral is not needed so need for loop
-            i, j = q.popleft()
-            for r, c in [(i+1, j), (i, j+1), (i, j-1), (i-1, j)]:
-                if 0 <= r < m and 0 <= c < n and board[r][c] == "O":
-                    board[r][c] = "#"
-                    q.append((r,c))
-                    #safe.add((r,c))
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                    if 0 <= r < m and 0 <= c < n and board[r][c] == 'O' and (r,c) not in safe :
+                        q.append((r,c))
+                        safe.add((r,c))
         
-        # Mark all remaining O to X and turn # to O
         for i in range(m):
             for j in range(n):
-                if board[i][j] == "O":
-                    board[i][j] = "X"
-                if board[i][j] == "#":
-                    board[i][j] = "O"
-                    
-
-                    
-                    
+                if board[i][j] == 'O' and (i,j) not in safe:
+                    board[i][j] = 'X'
+        
 
 
 
