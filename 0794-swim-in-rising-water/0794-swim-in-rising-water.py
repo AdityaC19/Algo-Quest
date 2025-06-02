ea2f7fa3dd09cb989884f56c7ed408b3e012c56e
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 18:18:25 -0400

    Time: 21 ms (88.89%), Space: 18.5 MB (37.57%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -1,26 +1,22 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        min_heap = [(grid[0][0], 0 ,0)]  # (time, i, j)
+        m, n = len(grid), len(grid[0])
+        min_heap = [(grid[0][0], 0, 0)]     #(time, i, j)
         seen = set()
 
+        res = 0
+        
         while min_heap:
             time, i, j = heapq.heappop(min_heap)
+            res = max(res, time)
 
-            if i == n-1 and j == n-1:
-                break
-
-            for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                if 0 <= r < n and 0 <= c < n and (r,c) not in seen:
-                    heapq.heappush(min_heap, (max(time, grid[r][c]), r,c))
-                    seen.add((r,c))
-        
-        return time
-
+            if (i, j) == (m-1, n-1):
+                return res
             
-
-
-
-        
+            for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                if 0 <= r < m and 0 <= c < n and (r,c) not in seen:
+                    heapq.heappush(min_heap, (grid[r][c], r, c))
+                    seen.add((r,c))
+         
 
         
