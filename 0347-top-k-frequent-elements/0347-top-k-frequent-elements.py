9e1a10c8296d50e34aee2700db40e769a10e8e1b
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-01 21:35:12 -0400

    Time: 3 ms (91.1%), Space: 21.1 MB (89.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,10 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hmap = defaultdict(int)
+        hmap = {}
         min_heap = []
 
         for num in nums:
-            hmap[num] += 1
+            if num in hmap:
+                hmap[num] += 1
+            else:
+                hmap[num] = 1
         
         for key,val in hmap.items():
             if len(min_heap) < k:
@@ -12,5 +15,7 @@ class Solution:
             else:
                 heapq.heappushpop(min_heap, (val, key))
         
-        return [h[1] for h in min_heap] 
+        return [h[1] for h in min_heap]
+
+
         
