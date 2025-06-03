521320d4757a6db85b8bc6843bf1e19c8a33318a
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-03 17:46:55 -0400

    Time: 96 ms (16.11%), Space: 32.8 MB (22.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,9 +4,9 @@ class Solution:
         max_summ = float('-inf')
 
         for i in range(len(nums)):
+            curr_summ = max(curr_summ, 0)
             curr_summ += nums[i]
             max_summ = max(max_summ, curr_summ)
-            curr_summ = max(curr_summ, 0)
         
         return max_summ
 
