07b38947b9bc9a926a22860115d956a33063a004
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-06-02 23:55:47 -0400

    Time: 281 ms (99.61%), Space: 21.5 MB (92.14%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -3,6 +3,7 @@ class Solution:
         n = len(points)
         seen = set()
         min_heap = [(0,0)]  #(dist, node)
+        cost = [float('inf')] * n
         res = 0
 
         while len(seen) < n:
@@ -17,7 +18,9 @@ class Solution:
                 if j not in seen:
                     xj, yj = points[j]
                     new_dist = abs(xi-xj) + abs(yi-yj)
-                    heapq.heappush(min_heap, (new_dist, j))
+                    if new_dist < cost[j]:
+                        cost[j] = new_dist
+                        heapq.heappush(min_heap, (new_dist, j))
         
         return res
 
