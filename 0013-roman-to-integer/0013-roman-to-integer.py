f9f87dd48ad9616bbc4eb978d600ff378b282d57
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-10-05 14:31:40 -0400

    Time: 6 ms (46.43%), Space: 17.7 MB (64.61%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,19 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        hmap = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
         i = 0
         n = len(s)
-        ans = 0
+        summ = 0
 
         while i < n:
-            if i < n-1 and dic[s[i]] < dic[s[i+1]]:
-                ans += dic[s[i+1]] - dic[s[i]]
+            if i < n-1 and hmap[s[i]] < hmap[s[i+1]]:
+                summ += hmap[s[i+1]] - hmap[s[i]]
                 i += 2
             else:
-                ans += dic[s[i]]
+                summ += hmap[s[i]]
                 i += 1
-        
-        return ans
 
         
+        return summ
+
+       
