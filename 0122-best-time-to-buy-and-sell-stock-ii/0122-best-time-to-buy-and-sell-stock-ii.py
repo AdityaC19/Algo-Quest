20270c0ff98c7121ffd04c7953b53281fb393514
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2025-10-05 16:31:28 -0400

    Time: 0 ms (100%), Space: 18.9 MB (45.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,36 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
-        max_profit = 0
-        n = len(prices)
-        ans = []
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        
+        return profit
+        
 
-        buy, sell = 0, 0
 
-        for i in range(n):
-            min_price = min(min_price, prices[i])
-            max_profit = max(max_profit, prices[i] - min_price)
-            ans.append(max_profit)
-            if max_profit > 0:
-                min_price = prices[i]
-                max_profit = 0
-        #print(ans)
-        summ = 0
-        for i in ans:
-            if i > 0:
-                summ += i
-        return summ
+
+        # min_price = prices[0]
+        # max_profit = 0
+        # n = len(prices)
+        # ans = []
+
+        # buy, sell = 0, 0
+
+        # for i in range(n):
+        #     min_price = min(min_price, prices[i])
+        #     max_profit = max(max_profit, prices[i] - min_price)
+        #     ans.append(max_profit)
+        #     if max_profit > 0:
+        #         min_price = prices[i]
+        #         max_profit = 0
+        # #print(ans)
+        # summ = 0
+        # for i in ans:
+        #     if i > 0:
+        #         summ += i
+        # return summ
         
 
         
