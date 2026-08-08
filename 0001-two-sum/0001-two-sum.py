4b58b43f044eb97212aea6e28b7b91ecfc65a4b0
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-08 12:14:18 -0700

    Time: 2 ms (56.49%), Space: 20.4 MB (40.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = defaultdict(int)
+        d = defaultdict(int)
 
         for i in range(len(nums)):
             x = target - nums[i]
-            if x in hmap:
-                return [hmap[x], i]
-            hmap[nums[i]] = i
+            if x in d:
+                return [i, d[x]]
+            d[nums[i]] = i
+
+            
+
+
+        
