8d28e15af9b36ec7a77333629e92dd75677a7257
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-12 15:16:17 -0700

    Time: 0 ms (100%), Space: 19.2 MB (56.62%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -4,16 +4,17 @@ class Solution:
         r = num
 
         while l <= r:
-            m = l + ((r-l)//2)
+            m = l + (r-l)//2
 
             if m*m == num:
                 return True
-            elif m*m < num:
-                l = m + 1
+            elif m*m > num:
+                r = m -1
             else:
-                r = m - 1
-        
+                l = m + 1
+            
         return False
+
 
 
         
