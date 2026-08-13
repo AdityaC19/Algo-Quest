bc7a1ec376d1860f723b860206417777b9d314ae
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-13 10:27:17 -0700

    Time: 127 ms (28.43%), Space: 19.7 MB (73.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,39 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        n = len(s)
-        formed = 0
         count1 = Counter(t)
         count2 = defaultdict(int)
         req = len(count1)
-        ans = float('inf')
-        res = ""
-        l = 0
-        r = 0
+        formed = 0
+        ans = ""
+        l = 0 
+        minLen = float('inf')
 
+        if len(s) < len(t): return ""
 
-
-        for r in range(n):
+        for r in range(len(s)):
             count2[s[r]] += 1
 
             if s[r] in count1 and count1[s[r]] == count2[s[r]]:
                 formed += 1
             
-            while formed == req:
-                if (r-l+1) < ans:
-                    res = s[l:r+1]
-                    ans = (r-l+1)
+            while req == formed:
+                if (r-l+1) < minLen:
+                    ans = s[l:r+1]
+                    minLen = (r-l+1)
                 
                 count2[s[l]] -= 1
 
-                if s[l] in count1 and count2[s[l]] < count1[s[l]]:
-                    formed -=1 
-                l+=1
+                if s[l] in count2 and count2[s[l]] < count1[s[l]]:
+                    formed -= 1
+                
+                l += 1
 
-        return res 
-
-
+        return ans
 
         
-
 
         
