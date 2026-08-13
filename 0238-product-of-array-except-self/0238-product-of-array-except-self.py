870dac6fb3dd896c15d094c0608ec82e4146a922
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-12 21:48:17 -0700

    Time: 34 ms (27.21%), Space: 27.8 MB (7.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,14 +3,32 @@ class Solution:
         n = len(nums)
         ans = [1] * n
 
-        pre = 1
-        for i in range(n):
-            ans[i] = pre
-            pre *= nums[i]
+        # prefix = 1
+        # for i in range(n):
+        #     ans[i] *= prefix
+        #     prefix *= nums[i]
         
-        post = 1
-        for i in range(n-1, -1, -1):
-            ans[i] *= post
-            post *= nums[i]
+        # post = 1
+        # for i in range(n-1, -1, -1):
+        #     ans[i] *= post
+        #     post *= nums[i]
+        
+        # return ans
+
+        pre = [1]*n
+        post = [1]*n
+
+        for i in range(1, n):
+            pre[i] = pre[i-1] * nums[i-1]
+        
+        for i in range(n-2, -1, -1):
+            post[i] = post[i+1] * nums[i+1]
+        
+        for i in range(n):
+            ans[i] = pre[i] * post[i]
         
         return ans
+
+
+
+              
