a27cff40c7ae4888a7080b01df67eab8be14b3e5
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-13 09:35:20 -0700

    Time: 24 ms (46.55%), Space: 25.1 MB (89.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,32 +3,35 @@ class Solution:
         n = len(nums)
         ans = [1] * n
 
-        # prefix = 1
-        # for i in range(n):
-        #     ans[i] *= prefix
-        #     prefix *= nums[i]
-        
-        # post = 1
-        # for i in range(n-1, -1, -1):
-        #     ans[i] *= post
-        #     post *= nums[i]
-        
-        # return ans
-
-        pre = [1]*n
-        post = [1]*n
-
-        for i in range(1, n):
-            pre[i] = pre[i-1] * nums[i-1]
-        
-        for i in range(n-2, -1, -1):
-            post[i] = post[i+1] * nums[i+1]
-        
+        prefix = 1
         for i in range(n):
-            ans[i] = pre[i] * post[i]
+            ans[i] *= prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(n-1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
         
         return ans
+        
 
 
 
-              
+        # n = len(nums)
+        # ans = [0]*n
+        # pre = [1]*n
+        # post = [1]*n
+
+
+        # for i in range(1,n):
+        #     pre[i] = pre[i-1] * nums[i-1]   # [1 1 2 6]
+        
+        # for i in range(n-2, -1, -1):
+        #     post[i] = post[i+1] * nums[i+1] # [24  12  4 1]
+        
+        # for i in range(n):
+        #     ans[i] = pre[i] * post[i]
+        
+        # return ans
+        
