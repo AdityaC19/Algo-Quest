3768dd9f29eb20944901bee0cbcbca0f32414b69
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-13 12:03:15 -0700

    Time: 101 ms (83.76%), Space: 77.3 MB (39.46%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -15,43 +15,89 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
     
-    def remove(self, node):
+    def deleteNode (self, node):
         prevNode = node.prev
         nextNode = node.next
         prevNode.next = nextNode
         nextNode.prev = prevNode
     
-    def insert(self, node):
+    def insertAfterHead(self, node):
         after = self.head.next
         node.prev = self.head
         node.next = after
         after.prev = node
         self.head.next = node
-        
-
+    
     def get(self, key: int) -> int:
         if key in self.cache:
             node = self.cache[key]
-            self.remove(node)
-            self.insert(node)
+            self.deleteNode(node)
+            self.insertAfterHead(node)
             return node.value
         return -1
-
-
+    
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             node.value = value
-            self.remove(node)
-            self.insert(node)
+            self.deleteNode(node)
+            self.insertAfterHead(node)
         else:
             node = Node(key, value)
+            self.insertAfterHead(node)
             self.cache[key] = node
-            self.insert(node)
             if len(self.cache) > self.capacity:
                 lru = self.tail.prev
-                self.remove(lru)
+                self.deleteNode(lru)
                 self.cache.pop(lru.key)
+
+
+
+
+
+
+
+
+
+
+    
+    # def remove(self, node):
+    #     prevNode = node.prev
+    #     nextNode = node.next
+    #     prevNode.next = nextNode
+    #     nextNode.prev = prevNode
+    
+    # def insert(self, node):
+    #     after = self.head.next
+    #     node.prev = self.head
+    #     node.next = after
+    #     after.prev = node
+    #     self.head.next = node
+        
+
+    # def get(self, key: int) -> int:
+    #     if key in self.cache:
+    #         node = self.cache[key]
+    #         self.remove(node)
+    #         self.insert(node)
+    #         return node.value
+    #     return -1
+
+
+    # def put(self, key: int, value: int) -> None:
+    #     if key in self.cache:
+    #         node = self.cache[key]
+    #         node.value = value
+    #         self.remove(node)
+    #         self.insert(node)
+    #     else:
+    #         node = Node(key, value)
+    #         self.cache[key] = node
+    #         self.insert(node)
+    #         if len(self.cache) > self.capacity:
+    #             lru = self.tail.prev
+    #             self.remove(lru)
+    #             self.cache.pop(lru.key)
             
 
 
