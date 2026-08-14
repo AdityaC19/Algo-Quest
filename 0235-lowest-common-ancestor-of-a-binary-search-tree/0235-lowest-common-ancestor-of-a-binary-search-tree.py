c6426a0dba9fcfd3cde66ab1ec03bddf612ac84e
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-13 19:01:18 -0700

    Time: 66 ms (50.53%), Space: 22.8 MB (18.15%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,17 +7,34 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        node = root
-
-        while node:
-            if node.val < p.val and node.val < q.val:
-                node = node.right
-            elif node.val > p.val and node.val > q.val:
-                node = node.left
-            else:
+        def dfs(node):
+            if not node:
+                return
+            
+            if node == p or node == q:
                 return node
-                #if node.left: q.append(node.left)
-                #if node.right: q.append(node.right)
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            # cond?
+            if left is not None and right is not None:
+                return node
+            
+            return left if left is not None else right
+
+        return dfs(root)     
         
+
+        # node = root
+
+        # while node:
+        #     if node.val < p.val and node.val < q.val:
+        #         node = node.right
+        #     elif node.val > p.val and node.val > q.val:
+        #         node = node.left
+        #     else:
+        #         return node
+
+
         
