cbf0916acf6ced3062a533b6172f731d509c66c4
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-14 14:50:01 -0700

    Time: 0 ms (100%), Space: 19.3 MB (86.9%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -9,15 +9,13 @@ class Solution:
         def dfs(p, q):
             if not p and not q:
                 return True
-            
             if not p or not q:
                 return False
-            
             if p.val != q.val:
                 return False
             
             return dfs(p.left, q.right) and dfs(p.right, q.left)
         
         return dfs(root, root)
-            
+
         
