359743879d83d29e1424682919fce1a20ba2f744
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-15 10:36:14 -0700

    Time: 3 ms (89.74%), Space: 22.9 MB (41.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,37 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
         counter = Counter(nums)
-        bucket = [0] * (n+1)
 
-        for key, freq in counter.items():
-            if bucket[freq] == 0:
-                bucket[freq] = [key]
+        min_heap = []
+
+        for key, val in counter.items():
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, (val, key))
             else:
-                bucket[freq].append(key)
+                heapq.heappushpop(min_heap, (val, key))
         
-        ans = []
-
-        for i in range(n, -1, -1):
-            if bucket[i] != 0:
-                ans.extend(bucket[i])
-            if len(ans) == k:
-                return ans
-        
-        
-
-
-        # hmap = defaultdict(int)
-        # min_heap = []
-
-        # for i in range(len(nums)):
-        #     hmap[nums[i]] += 1
-
-        # for key, value in hmap.items():
-        #     if len(min_heap) < k:
-        #         heapq.heappush(min_heap, (value, key)) 
-        #     else:
-        #         heapq.heappushpop(min_heap, (value, key))
-        
-        # return [h[1] for h in min_heap]
+        return [h[1] for h in min_heap]
         
