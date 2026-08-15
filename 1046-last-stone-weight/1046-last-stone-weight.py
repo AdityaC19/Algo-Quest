cf3df96627d25c157abc6d7b1901ba9d18de9732
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-15 10:28:25 -0700

    Time: 0 ms (100%), Space: 19.4 MB (35.53%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,9 +1,7 @@
-import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         n = len(stones)
 
-        # max heap
         for i in range(n):
             stones[i] = -stones[i]
         
@@ -17,10 +15,7 @@ class Solution:
                 heapq.heappush(stones, largest - second)
             
         if len(stones) == 1:
-            return -(heapq.heappop(stones))
+            return -stones[0]
         else:
             return 0
-        
-
-        
         
