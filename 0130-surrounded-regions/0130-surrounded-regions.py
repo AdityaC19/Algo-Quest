3b569f7ef33a2e120e9525bd42084615d05b05ce
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 16:29:15 -0700

    Time: 7 ms (50.64%), Space: 22.3 MB (59.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -7,6 +7,7 @@ class Solution:
         dont_touch = set()
         q = deque()
 
+        # add perimeter Os in safe set
         for i in range(m):
             for j in range(n):
                 if i ==0 or i == m-1 or j == 0 or j == n-1:
@@ -14,6 +15,7 @@ class Solution:
                         dont_touch.add((i, j))
                         q.append((i,j))
 
+        # use BFS to reach for other Os from perimeter
         while q:
             i, j = q.popleft()
             for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
@@ -21,7 +23,8 @@ class Solution:
                     if (r,c) not in dont_touch and board[r][c] == 'O':
                         q.append((r,c))
                         dont_touch.add((r,c))
-    
+
+        # turn rest of the Os into Xs which are not in safe set
         for i in range(m):
             for j in range(n):
                 if (i,j) not in dont_touch and board[i][j] == 'O':
