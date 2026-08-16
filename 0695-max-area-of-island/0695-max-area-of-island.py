c4d391ab0c76914a2dd1d82df4842a0a76af4283
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 11:08:25 -0700

    Time: 22 ms (42.86%), Space: 20.3 MB (64.18%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
         area = 0
-        max_area = 0
 
-        def dfs(i,j):
+        def dfs(i, j):
             nonlocal area
-            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != 1:
-                return 0
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != 1:
+                return 
             
             grid[i][j] = '#'
             area += 1
@@ -17,12 +17,13 @@ class Solution:
             dfs(i-1, j)
             dfs(i, j-1)
         
+        maxArea = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     area = 0
-                    dfs(i,j)
-                max_area = max(max_area, area)
-        
-        return max_area
+                    dfs(i, j)
+                maxArea = max(maxArea, area)
+
+        return maxArea
         
