bc349f7cbc53d12f709ba1261757e6d3ea76fc65
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 12:07:06 -0700

    Time: 3 ms (86.12%), Space: 21 MB (38.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,45 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        indeg = [0] * numCourses
 
         for u, v in prerequisites:
-            graph[v].append(u)
-            indeg[u] += 1
+            graph[u].append(v)
         
-        q = deque([i for i in range(numCourses) if indeg[i] == 0])
-        res = []
+        visited = [0] * numCourses
 
-        while q:
-            node = q.popleft()
-            res.append(node)
-            for nei_node in graph[node]:
-                indeg[nei_node] -= 1
-                if indeg[nei_node] == 0:
-                    q.append(nei_node)
-        
-        return True if len(res) == numCourses else False
-        
-
-        
-
-        # visited = [0] * numCourses
-
-        # def dfs(i):
-        #     if visited[i] == 1: return True
-        #     if visited[i] == 2: return False
-
-        #     visited[i] = 1
-
-        #     for nei_node in graph[i]:
-        #         if dfs(nei_node):
-        #             return True
+        def dfs(node):
+            if visited[node] == 1:  # visiting again (cycle detected)
+                return True
+            if visited[node] == 2: # visited
+                return False
             
-        #     visited[i] = 2
-        #     return False
+            visited[node] = 1
+
+            for nei_node in graph[node]:
+                if dfs(nei_node):
+                    return True
+            
+            visited[node] = 2
+            return False
         
-        # for i in range(numCourses):
-        #     if not visited[i]:
-        #         if dfs(i):
-        #             return False
-        # return True
+        for i in range(numCourses):
+            if dfs(i):
+                return False
+            
+        return True
+            
