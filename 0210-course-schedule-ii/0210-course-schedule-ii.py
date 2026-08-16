acf98cbcee2ea5a34b2f4bc47c0fe470e9650e12
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 13:16:01 -0700

    Time: 4 ms (47.99%), Space: 20.2 MB (98.06%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,34 +1,54 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+        indeg = [0] * numCourses
 
         for u, v in prerequisites:
-            graph[u].append(v)
-        
-        visited = [0] * numCourses
-        order = []
-        
-        def dfs(node):
-            if visited[node] == 1:
-                return True
-            if visited[node] == 2:
-                return False
-            
-            visited[node] = 1
+            graph[v].append(u)
+            indeg[u] += 1
 
-            for nei_node in graph[node]:
-                if dfs(nei_node):
-                    return True
-            
-            visited[node] = 2
-            order.append(node)
-            
-            return False
-        
+        q = deque()
+
         for i in range(numCourses):
-            dfs(i)
+            if indeg[i] == 0:
+                q.append(i)
+        
+        order = []
+
+        while q:
+            node = q.popleft()
+            order.append(node)
+            for nei_node in graph[node]:
+                indeg[nei_node] -=1
+                if indeg[nei_node] == 0:
+                    q.append(nei_node)
         
         return order if len(order) == numCourses else []
+
+        # visited = [0] * numCourses
+        # order = []
+        
+        # def dfs(node):
+        #     if visited[node] == 1:
+        #         return True
+        #     if visited[node] == 2:
+        #         return False
+            
+        #     visited[node] = 1
+
+        #     for nei_node in graph[node]:
+        #         if dfs(nei_node):
+        #             return True
+            
+        #     visited[node] = 2
+        #     order.append(node)
+            
+        #     return False
+        
+        # for i in range(numCourses):
+        #     dfs(i)
+        
+        # return order if len(order) == numCourses else []
         
 
 
