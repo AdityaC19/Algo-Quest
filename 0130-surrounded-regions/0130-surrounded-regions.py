2821faf0085427a30cf2dc6ced2beda73e96057f
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 16:27:12 -0700

    Time: 7 ms (50.64%), Space: 22.4 MB (59.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,39 +4,33 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         m, n = len(board), len(board[0])
+        dont_touch = set()
         q = deque()
-        safe = set()
 
-        # Mark all boundary O as safe:
         for i in range(m):
-            for j in [0, n-1]:  # first and last col
-                if board[i][j] == 'O':
-                    q.append((i,j))
-                    safe.add((i,j))
-        
-        for i in [0, m-1]:  # First and last row
             for j in range(n):
-                if board[i][j] == 'O':
-                    q.append((i,j))
-                    safe.add((i,j))
-        
-        # Use BFS to mark safe Os
+                if i ==0 or i == m-1 or j == 0 or j == n-1:
+                    if board[i][j] == 'O':
+                        dont_touch.add((i, j))
+                        q.append((i,j))
+
         while q:
             for _ in range(len(q)):
                 i, j = q.popleft()
                 for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and board[r][c] == 'O' and (r,c) not in safe :
-                        q.append((r,c))
-                        safe.add((r,c))
+                    if 0 <= r < m and 0 <= c < n:
+                        if (r,c) not in dont_touch and board[r][c] == 'O':
+                            q.append((r,c))
+                            dont_touch.add((r,c))
         
         for i in range(m):
             for j in range(n):
-                if board[i][j] == 'O' and (i,j) not in safe:
+                if (i,j) not in dont_touch and board[i][j] == 'O':
                     board[i][j] = 'X'
+
+
+                
         
 
-
-
-
-
         
+
