d2feaa98d6d55bb727cc10a5b9cb11fe03d6957d
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-16 10:42:22 -0700

    Time: 239 ms (69.77%), Space: 21.6 MB (72.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        islands = 0
-
-        seen = set()
-
-        def bfs(i, j):
-            q = deque([(i,j)])
-
-            while q:
-                i,j = q.popleft()
-                for r, c in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == '1':
-                        q.append((r,c))
-                        seen.add((r,c))
+        m,n = len(grid), len(grid[0])
         
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+                return 
+            
+            grid[i][j] = '#'
+
+            dfs(i+1, j)
+            dfs(i, j+1)
+            dfs(i-1, j)
+            dfs(i, j-1)
+        
+        num_islands = 0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in seen and grid[i][j] == '1':
-                    seen.add((i,j))
-                    islands += 1
-                    bfs(i,j)
-
-        return islands
-    
-
-
+                if grid[i][j] == '1':
+                    num_islands+=1
+                    dfs(i,j)
         
+        return num_islands
