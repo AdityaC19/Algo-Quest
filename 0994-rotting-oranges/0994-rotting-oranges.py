ea1705b064248de9107fbf3f51ffae0793ab5dec
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-17 11:23:30 -0700

    Time: 3 ms (74.69%), Space: 19.3 MB (78.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,21 +1,21 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        m , n = len(grid), len(grid[0])
-        seen = set()
-        time = 0
+        m, n = len(grid), len(grid[0])
         fresh_oranges = 0
+        seen = set()
+        q = deque()
 
-        # multi-source BFS
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i,j))
                     seen.add((i,j))
                 if grid[i][j] == 1:
-                    fresh_oranges += 1
-        
+                    fresh_oranges +=1
+
         if fresh_oranges == 0: return 0
+
+        time = 0
         while q:
             time += 1
             for _ in range(len(q)):
@@ -23,16 +23,14 @@ class Solution:
                 for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
                     if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == 1:
                         grid[r][c] = 2
-                        fresh_oranges -= 1
+                        fresh_oranges -=1
+                        if fresh_oranges == 0:
+                            return time
                         q.append((r,c))
                         seen.add((r,c))
-                if fresh_oranges == 0:
-                    return time
-
+            
         return -1
-                
-
-
+        
 
 
 
