f6dbb630855d0229fd47b2209a5489cbfb4cb295
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-17 11:12:27 -0700

    Time: 24 ms (82.18%), Space: 20.7 MB (88.14%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,30 +1,32 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacq = deque()
+        atlq = deque()
+
         pseen = set()
         aseen = set()
+
         m, n = len(heights), len(heights[0])
-
         # left + top
-        pac = deque([(i, 0) for i in range(m)] + [(0, j) for j in range(n)])
+        pacq = deque([(i,0) for i in range(m)] + [(0, j) for j in range(n)])
         # right + bottom
-        atl = deque([(i, n-1) for i in range(m)] + [(m-1, j) for j in range(n)])
+        atlq = deque([(i, n-1) for i in range(m)] + [(m-1, j) for j in range(n)])
 
-        pseen = set(pac)
-        aseen = set(atl)
+        pseen = set(pacq)
+        aseen = set(atlq)
 
         def bfs(q, seen):
             while q:
-                for _ in range(len(q)):
-                    (i,j) = q.popleft()
-                    for r, c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                        if 0 <= r < m and 0 <= c < n and (r,c) not in seen and heights[r][c] >= heights[i][j]:
-                            q.append((r,c))
-                            seen.add((r,c))
+                i, j = q.popleft()
+                for r,c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
+                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and heights[r][c] >= heights[i][j]:
+                        q.append((r,c))
+                        seen.add((r,c))
         
-        bfs(pac, pseen)
-        bfs(atl, aseen)
+        bfs(pacq, pseen)
+        bfs(atlq, aseen)
+
         return list(pseen & aseen)
 
 
 
-        
