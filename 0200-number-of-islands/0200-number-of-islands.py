f16b885f3445112d3c31d1270a63a3d00589cec2
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-19 16:31:51 -0700

    Time: 235 ms (80.49%), Space: 21.5 MB (91.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        num_islands = 0
-        seen = set()
+        m,n = len(grid), len(grid[0])
 
-        def bfs(i, j):
-            q = deque([(i,j)])
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >=n or grid[i][j] != '1':
+                return
+            
+            grid[i][j] = '0'
 
-            while q:
-                i, j = q.popleft()
-                for r,c in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                    if 0 <= r < m and 0 <= c < n and (r,c) not in seen and grid[r][c] == '1':
-                        q.append((r,c))
-                        seen.add((r,c))
+            dfs(i+1, j)
+            dfs(i, j+1)
+            dfs(i-1, j)
+            dfs(i, j-1)
         
+        count = 0
         for i in range(m):
             for j in range(n):
-                if (i,j) not in seen and grid[i][j] == '1':
-                    num_islands += 1
-                    bfs(i, j)
+                if grid[i][j] == '1':
+                    dfs(i,j)
+                    count += 1
         
-        return num_islands
+        return count
+
         
