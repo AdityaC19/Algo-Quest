9776b902ec7a43eb2f483c7ad891482cdb6b437e
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-20 14:09:06 -0700

    Time: 3 ms (84.97%), Space: 21.1 MB (46.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        counter = Counter(nums)
+        n = len(nums)
+
+        for key,val in counter.items():
+            if val > (n//2):
+                return key
+        
