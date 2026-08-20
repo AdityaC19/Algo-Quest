2b4542971413c0d86f1b5e0910582a08bdfa6511
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-20 15:01:19 -0700

    Time: 135 ms (23.81%), Space: 19.9 MB (11.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        count1 = Counter(t)
-        count2 = defaultdict(int)
-        req = len(count1)
-        formed = 0
-        ans = ""
-        l = 0 
-        minLen = float('inf')
-
         if len(s) < len(t): return ""
 
-        for r in range(len(s)):
-            count2[s[r]] += 1
+        count_s = defaultdict(int)
+        count_t = Counter(t)
+        req = len(count_t)
+        formed = 0
+        ans = ""
+        l, r= 0, 0
+        minLen = float('inf')
 
-            if s[r] in count1 and count1[s[r]] == count2[s[r]]:
+        for r in range(len(s)):
+            count_s[s[r]] += 1
+
+            if s[r] in count_t and count_s[s[r]] == count_t[s[r]]:
                 formed += 1
-            
+
             while req == formed:
                 if (r-l+1) < minLen:
                     ans = s[l:r+1]
-                    minLen = (r-l+1)
+                    minLen = min(minLen, (r-l+1))
                 
-                count2[s[l]] -= 1
-
-                if s[l] in count2 and count2[s[l]] < count1[s[l]]:
+                count_s[s[l]] -= 1
+                
+                if s[l] in count_t and count_s[s[l]] < count_t[s[l]]:
                     formed -= 1
                 
                 l += 1
-
+        
         return ans
-
-        
-
-        
