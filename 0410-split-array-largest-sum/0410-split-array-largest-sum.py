db323e2d7b00bd2e941dfa5de397d880800659af
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-21 15:49:40 -0700

    Time: 0 ms (100%), Space: 19.4 MB (46.36%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def helper(nums, max_sum, k):
-            count = 1
+        def helper(max_sum, k):
+            subarray = 1
             cur_sum = 0
             for num in nums:
                 cur_sum += num
                 if cur_sum > max_sum:
-                    count += 1
+                    subarray += 1
                     cur_sum = num
-                    if count > k:
+                    if subarray > k:
                         return False
             return True
         
@@ -17,7 +17,7 @@ class Solution:
 
         while l <= r:
             m = l + (r-l)//2
-            if helper(nums, m, k):
+            if helper(m, k):
                 ans = m 
                 r = m - 1
             else:
