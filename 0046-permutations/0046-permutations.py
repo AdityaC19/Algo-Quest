f5d561a87b2a1cccecadbe7b6952944a6ad5b80c
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-21 09:27:08 -0700

    Time: 0 ms (100%), Space: 19.4 MB (87.61%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -6,14 +6,13 @@ class Solution:
 
         def backtrack():
             if len(sol) == n:
-                res.append(sol[:])
-                return
-
+                return res.append(sol[:])
+            
             for num in nums:
                 if num not in sol:
                     sol.append(num)
                     backtrack()
                     sol.pop()
-        
         backtrack()
         return res
+        
