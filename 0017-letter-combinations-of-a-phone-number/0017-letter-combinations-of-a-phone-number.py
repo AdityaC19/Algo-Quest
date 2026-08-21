f4ff7de6f93d2a51d10db382f976d888554b4d8a
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-20 17:28:45 -0700

    Time: 0 ms (100%), Space: 19.4 MB (9.63%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        keyboard = {
+        phone_board = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -10,22 +10,23 @@ class Solution:
             "8": "tuv",
             "9": "wxyz"
             }
-        
-        res = []
-        sol = []
 
-        if len(digits) == 0: return []
+        sol = []
+        res = []
+        n = len(digits)
+        
+        if n == 0: return []
 
         def backtrack(i):
-            if len(sol) == len(digits):
+            if len(sol) == n:
                 res.append(''.join(sol[:]))
                 return
             
-            for x in keyboard[digits[i]]:
+            for x in phone_board[digits[i]]:
                 sol.append(x)
                 backtrack(i+1)
                 sol.pop()
-        
+
         backtrack(0)
         return res
-                    
+        
