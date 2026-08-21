27cf0d0add57a9c2cc3af6e8fac49cf98779b53d
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-20 22:47:27 -0700

    Time: 30 ms (50.12%), Space: 30.4 MB (5.14%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,36 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = defaultdict(set)
+
+        for account in accounts:
+            for email in account[1:]:
+                graph[email].add(account[1])
+                graph[account[1]].add(email)
+        
+        def dfs(node, res):
+            if node in seen:
+                return
+            
+            seen.add(node)
+            for nei_node in graph[node]:
+                if nei_node not in seen:
+                    dfs(nei_node, res)
+
+            res.append(node)
+        
+        seen = set()
+        ans = []
+
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                if email not in seen:
+                    res = []
+                    dfs(email, res)
+                    if res:
+                        ans.append([name] + sorted(res))
+        
+        return ans
+
+
+        
