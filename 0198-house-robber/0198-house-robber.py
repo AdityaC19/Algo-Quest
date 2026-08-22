753e896617e6ab0edf5c67272191386d1f5e15d6
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-21 17:45:52 -0700

    Time: 0 ms (100%), Space: 19.4 MB (23.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,7 +6,7 @@ class Solution:
             return nums[0]
         if n == 2:
             return max(nums[0], nums[1])
-
+        
         dp = [0] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
@@ -15,6 +15,5 @@ class Solution:
             dp[i] = max(dp[i-2] + nums[i], dp[i-1])
         
         return dp[n-1]
-
 
         
