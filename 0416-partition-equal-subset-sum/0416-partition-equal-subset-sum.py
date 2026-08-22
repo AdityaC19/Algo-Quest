420af49a59f5934e025d3c10fa1eeff8f689a913
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-21 18:13:17 -0700

    Time: 631 ms (73.05%), Space: 163.8 MB (14.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        ans = []
 
         total = sum(nums)
 
         if total % 2 != 0:
             return False
         
-        target = total // 2
+        target =  total // 2
 
         @cache
         def dfs(i, curSum):
@@ -20,9 +19,4 @@ class Solution:
             
             return dfs(i+1, curSum + nums[i]) or dfs(i+1, curSum)
         
-        return dfs(0,0)
-
-
-
-
-        
+        return dfs(0, 0)
