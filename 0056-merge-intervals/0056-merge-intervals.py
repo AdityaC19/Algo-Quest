d3195a985e9d2a61a07b7665cc82d868ea460d94
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-22 20:05:29 -0700

    Time: 7 ms (72.34%), Space: 22.5 MB (59.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,7 +2,7 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         ans = [intervals[0]]
-
+        
         for i in range(1, len(intervals)):
             curr = ans[-1]
 
