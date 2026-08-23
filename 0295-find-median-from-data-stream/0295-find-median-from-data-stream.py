c707dee8aaf44ebe13805380b696c1ef1a3f8309
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-22 19:20:50 -0700

    Time: 229 ms (19.41%), Space: 41.6 MB (87.69%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,32 +1,26 @@
 class MedianFinder:
 
     def __init__(self):
-        self.small = []     # maxHeap
-        self.large = []     # minHeap
-        
-    def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -num)
-        # val in small > val in large heap
-        if self.small and self.large and -self.small[0] > self.large[0]:
-            val = -heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
+        self.max_heap = []   # store smaller numbers
+        self.min_heap = []   # store larger numbers  
 
-        #uneven lengths
-        if len(self.small) > len(self.large) + 1:
-            val = -heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
-        if len(self.large) > len(self.small) + 1:
-            val = heapq.heappop(self.large)
-            heapq.heappush(self.small, -val)
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.max_heap, -num)
+
+        x = -heapq.heappop(self.max_heap)
+        heapq.heappush(self.min_heap, x)
+
+        if len(self.max_heap) < len(self.min_heap):
+            y = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -y)
         
+
     def findMedian(self) -> float:
-        if len(self.small) > len(self.large):
-            return -self.small[0]
-        if len(self.large) > len(self.small):
-            return self.large[0]
-        else:      
-            return (-self.small[0] + self.large[0]) / 2 
-            
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return (self.min_heap[0] - self.max_heap[0]) / 2.0
+
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
