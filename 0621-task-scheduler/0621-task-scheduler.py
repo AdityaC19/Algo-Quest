7a7d330699746769aae693d9f586473640d568ba
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-24 10:55:44 -0700

    Time: 166 ms (34.37%), Space: 20.9 MB (38.57%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,29 +1,31 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         counter = Counter(tasks)
-        max_heap = []   #(freq)
-        wait_q = deque()    #(tasks, when available time)
-        time = 0
+        max_heap = []
+        q = deque()
+        intervals = 0
 
-        for task in counter:
-            heapq.heappush(max_heap, -counter[task])
+        for key, val in counter.items():
+            heapq.heappush(max_heap, -val)
         
-        while max_heap or wait_q:
-            time += 1
+        while max_heap or q:
+            intervals += 1
             if max_heap:
-                task = heapq.heappop(max_heap)
-                task = -task - 1
+                freq =  heapq.heappop(max_heap)
+                freq = -freq - 1
 
-                if task > 0:
-                    wait_q.append((task, time + n))
+                if freq > 0:
+                    q.append((freq, intervals + n))
+
+            if q and q[0][1] == intervals:
+                f = -q.popleft()[0]
+                heapq.heappush(max_heap, f)
+
+        return intervals
             
-            if wait_q and wait_q[0][1] == time:
-                task_count = wait_q.popleft()[0]
-                heapq.heappush(max_heap, -task_count)
-        
-        return time
 
 
-                
+
+
 
         
