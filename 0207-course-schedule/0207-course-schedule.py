5eec6b0e05231bb69e218169f44e3ae15e0cd1af
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-24 23:07:58 -0700

    Time: 3 ms (85.87%), Space: 21.2 MB (25.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,9 +2,9 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
 
-        for u, v in prerequisites:
+        for u,v in prerequisites:
             graph[u].append(v)
-
+        
         visited = [0] * numCourses
 
         def dfs(node):
@@ -21,10 +21,11 @@ class Solution:
             
             visited[node] = 2
             return False
-
+        
         for i in range(numCourses):
             if dfs(i):
                 return False
-            
+        
         return True
 
+        
