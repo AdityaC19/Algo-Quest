70c42f2f5b849c80c4f60d1b5139182032c380a5
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-24 23:16:42 -0700

    Time: 0 ms (100%), Space: 20.2 MB (90.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -3,27 +3,33 @@ class Solution:
         graph = defaultdict(list)
         indeg = [0] * numCourses
 
-        for u,v in prerequisites:
+        for u, v in prerequisites:
             graph[v].append(u)
             indeg[u] += 1
-
+        
         q = deque()
-
+        seen = set()
+        
         for i in range(numCourses):
             if indeg[i] == 0:
                 q.append(i)
-        
+                seen.add(i)
+
         order = []
         while q:
             node = q.popleft()
             order.append(node)
-
             for nei_node in graph[node]:
-                indeg[nei_node] -= 1
-                if indeg[nei_node] == 0:
-                    q.append(nei_node)
+                if nei_node not in seen:
+                    indeg[nei_node] -= 1
+                    if indeg[nei_node] == 0:
+                        q.append(nei_node)
         
         return order if len(order) == numCourses else []
+        
+
+                
+                    
 
 
         
