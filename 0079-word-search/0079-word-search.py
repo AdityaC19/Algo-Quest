ad550b78eb9ee8db8b46342b01cc71dabfdfb4ec
AdityaC19 <46404242+AdityaC19@users.noreply.github.com>
2026-08-25 16:59:59 -0700

    Time: 3128 ms (87.82%), Space: 19.6 MB (8.45%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,13 +2,12 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
 
-
         def dfs(i, j, k):
-            if k == len(word):
-                return True
-            
-            if i < 0 or i >=m or j < 0 or j >= n or board[i][j] != word[k]:
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
                 return False
+            
+            if k == len(word)-1:
+                return True
             
             char = board[i][j]
             board[i][j] = '#'
@@ -20,10 +19,13 @@ class Solution:
         
         for x in range(m):
             for y in range(n):
-                if dfs(x,y,0):
+                if dfs(x, y, 0):
                     return True
         
         return False
+            
+
+
             
 
         
